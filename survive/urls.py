664440eea74a51d9191442e7411e17a1263ba734
mawilson <mawilson2012@gmail.com>
2023-11-11 02:05:57 -0800

    Survivor start (#2)
    * Initial model support & layout layout
    
    * Grid view of survivor teams

---
+++ survive/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from survive import views
+from survive.models import Team
+
+home_list_view = views.HomeListView.as_view(
+    queryset = Team.objects.order_by("name"), # alphabetic sort by Team Name, ascending
+    context_object_name = "team_list",
+    template_name = "survive/home.html",
+)
+
+urlpatterns = [
+    # path("", views.home, name="home"),
+    path("", home_list_view, name="home"),
+    path("add_team/", views.add_team, name="add_team")
+]
---
+++ survive/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from survive.forms import TeamForm
+from survive.models import Team
+from django.views.generic import ListView
+
+class HomeListView(ListView):
+    """Renders the home page, with a list of all teams."""
+    model = Team
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
+
+# Create your views here.
+
+# def home(request):
+#     return render(request, "survive/home.html")
+
+def add_team(request):
+    form = TeamForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            team = form.save(commit=True)
+            return redirect("home")
+    else:
+        return render(request, "survive/add_team.html", {"form": form})
