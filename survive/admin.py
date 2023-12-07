38eb0071cc41b0f01900cfdb4b9e6e3c20ae7560
Matt Wilson <mawilson2012@gmail.com>
2023-12-06 17:11:06 -0800

    Registered Survivor admin models

--- survive/admin.py
+++ survive/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Survivor)
+admin.site.register(Season)
+admin.site.register(Rubric)
+admin.site.register(Team)
--- web_project/urls.py
+++ web_project/urls.py
@@ -17,11 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
 urlpatterns = [
     # path("", include("hello.urls")),
     path("", include("survive.urls")),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
 ]
 
 urlpatterns += staticfiles_urlpatterns()
