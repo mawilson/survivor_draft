72d83f68cdca835e11e0280b12bce2f47607031a
Matt Wilson <mawilson2012@gmail.com>
2023-12-29 17:13:59 -0800

    Added season selector Added season selector Adjusted home page to use selected season Adjusted fan favorite page to use selected season

--- survive/forms.py
+++ survive/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from survive.models import Team
+from survive.models import Team, Season
 
 class TeamForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,8 @@ class FanFavoriteForm(forms.ModelForm):
     class Meta:
         model = Team
         fields = ("fan_favorite_first", "fan_favorite_second", "fan_favorite_third", "fan_favorite_bad",)
+
+class SeasonForm(forms.ModelForm):
+    class Meta:
+        model = Season
+        fields = ("name",)
--- survive/views.py
+++ survive/views.py
@@ -15,8 +15,28 @@ class HomeListView(ListView):
 
 # Create your views here.
 
-# def home(request):
-#     return render(request, "survive/home.html")
+def home(request):
+    season_id = request.COOKIES.get("season_id") # if season id has been set before, get it & use it for context
+    context = {
+        "seasons": Season.objects.all()
+    }
+    if season_id:
+        context["season"] = Season.objects.get(id=season_id)
+    else:
+        context["season"] = Season.objects.all()[0] # just use the first Season in the DB, if it exists
+
+    new_season_id = None
+    if request.method == "GET":
+        new_season_id = request.GET.get("season_id")
+        if new_season_id: # if new_season_id is present, it was provided via the Season selector, update cookie for it & the context
+            context["season"] = Season.objects.get(id=new_season_id)
+
+    response = render(request, "survive/home.html", context)
+    if new_season_id:
+        response.set_cookie("season_id", new_season_id)
+    return response
+        
+
 
 def add_team(request):
     form = TeamForm(request.POST or None)
@@ -33,11 +53,21 @@ def survivor(request, id):
     return render(request, "survive/survivor.html", context)
 
 def fan_favorite(request):
+    season_id = request.COOKIES.get("season_id") # if season id has been set before, get it & use it for context
     context = {
         "form": FanFavoriteForm(request.POST or None),
-        "teams": Team.objects.all(),
-        "season": Season.objects.all()[0]
+        "seasons": Season.objects.all()
     }
+    if season_id:
+        context["season"] = Season.objects.get(id=season_id)
+    else:
+        context["season"] = Season.objects.all()[0] # just use the first Season in the DB, if it exists
+
+    new_season_id = None
+    if request.method == "GET":
+        new_season_id = request.GET.get("season_id")
+        if new_season_id: # if new_season_id is present, it was provided via the Season selector, update cookie for it & the context
+            context["season"] = Season.objects.get(id=new_season_id)
 
     if request.method == "POST":     
         selected_team = get_object_or_404(Team, pk = request.POST.get("team_id"))
@@ -47,6 +77,9 @@ def fan_favorite(request):
             selected_team.season.fan_favorites() # will evaluate all votes & assign Survivors accordingly
             return redirect("./") # after submitting, redirect to same page to refresh
         else:
-            return render(request, "survive/fan_favorite_vote.html", {"form": form, "teams": context["teams"], "selected_team": selected_team.id, "season": context["season"]})
+            return render(request, "survive/fan_favorite_vote.html", {"form": form, "selected_team": selected_team.id, "season": context["season"]})
     else:
-        return render(request, "survive/fan_favorite_vote.html", context)
+        response = render(request, "survive/fan_favorite_vote.html", context)
+        if new_season_id:
+            response.set_cookie("season_id", new_season_id)
+        return response
