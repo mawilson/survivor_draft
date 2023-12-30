692466eaa4b34cd020b58e3b7013a76a18ea002f
Matt Wilson <mawilson2012@gmail.com>
2023-12-30 01:25:04 -0800

    Fixes to season selector integration Fixed fan favorite vote page for voteless faves Made fan favorite view logic more sensible Fixed bug in fan_favorites calculation Added save param to fan_favorites function Reset fan favorite form on init for anonymity

--- survive/forms.py
+++ survive/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from survive.models import Team, Season
+from survive.models import Team, Season, Survivor
 
 class TeamForm(forms.ModelForm):
     class Meta:
@@ -10,6 +10,15 @@ class FanFavoriteForm(forms.ModelForm):
     class Meta:
         model = Team
         fields = ("fan_favorite_first", "fan_favorite_second", "fan_favorite_third", "fan_favorite_bad",)
+
+    def __init__(self, *args, **kwargs):
+        super(FanFavoriteForm, self).__init__(*args, **kwargs)
+        if self.instance:
+            season_id = self.instance.season.id if self.instance.season else None
+            self.fields["fan_favorite_first"].queryset = Survivor.objects.filter(season__id=season_id)
+            self.fields["fan_favorite_second"].queryset = Survivor.objects.filter(season__id=season_id)
+            self.fields["fan_favorite_third"].queryset = Survivor.objects.filter(season__id=season_id)
+            self.fields["fan_favorite_bad"].queryset = Survivor.objects.filter(season__id=season_id)
 
 class SeasonForm(forms.ModelForm):
     class Meta:
--- survive/views.py
+++ survive/views.py
@@ -23,7 +23,7 @@ def home(request):
     if season_id:
         context["season"] = Season.objects.get(id=season_id)
     else:
-        context["season"] = Season.objects.all()[0] # just use the first Season in the DB, if it exists
+        context["season"] = Season.objects.first() # just use the first Season in the DB, if it exists
 
     new_season_id = None
     if request.method == "GET":
@@ -55,13 +55,12 @@ def survivor(request, id):
 def fan_favorite(request):
     season_id = request.COOKIES.get("season_id") # if season id has been set before, get it & use it for context
     context = {
-        "form": FanFavoriteForm(request.POST or None),
         "seasons": Season.objects.all()
     }
     if season_id:
         context["season"] = Season.objects.get(id=season_id)
     else:
-        context["season"] = Season.objects.all()[0] # just use the first Season in the DB, if it exists
+        context["season"] = Season.objects.first() # just use the first Season in the DB, if it exists
 
     new_season_id = None
     if request.method == "GET":
@@ -69,15 +68,25 @@ def fan_favorite(request):
         if new_season_id: # if new_season_id is present, it was provided via the Season selector, update cookie for it & the context
             context["season"] = Season.objects.get(id=new_season_id)
 
+    team = context["season"].team_set.first()
+    initial_data = {"fan_favorite_first": None, "fan_favorite_second": None, "fan_favorite_third": None, "fan_favorite_bad": None}
+    context["form"] = FanFavoriteForm(request.POST or None, instance = team, initial = initial_data)
+
     if request.method == "POST":     
         selected_team = get_object_or_404(Team, pk = request.POST.get("team_id"))
         form = FanFavoriteForm(context["form"].data, instance = selected_team) # can't change the existing form's instance, but can make a new one with identical data
         if form.is_valid():
             form.save(commit=True)
-            selected_team.season.fan_favorites() # will evaluate all votes & assign Survivors accordingly
+            selected_team.season.fan_favorites(save=True) # will evaluate all votes & assign Survivors accordingly
             return redirect("./") # after submitting, redirect to same page to refresh
         else:
-            return render(request, "survive/fan_favorite_vote.html", {"form": form, "selected_team": selected_team.id, "season": context["season"]})
+            new_context = {
+                "form": form,
+                "selected_team": selected_team.id,
+                "season": context["season"],
+                "seasons": context["seasons"]
+            }
+            return render(request, "survive/fan_favorite_vote.html", new_context)
     else:
         response = render(request, "survive/fan_favorite_vote.html", context)
         if new_season_id:
