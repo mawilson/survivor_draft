f3d373df6043ea645abcca59922813f9d5ad10f4
Matt Wilson <mawilson2012@gmail.com>
2024-01-11 20:08:33 -0800

    Added login page & URL pattern Added login page Added login to navbar Styled navbar as a table Added dummy password reset page

--- survive/urls.py
+++ survive/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from survive import views
 from survive.models import Team
+from django.contrib.auth import views as auth_views
 
 home_list_view = views.HomeListView.as_view(
     queryset = Team.objects.order_by("name"), # alphabetic sort by Team Name, ascending
@@ -14,5 +15,7 @@ urlpatterns = [
     # path("add_team/", views.add_team, name="add_team") # deprecated
     path("survivor/<int:id>/", views.survivor, name="survivor_page"),
     path("fan_favorite", views.fan_favorite, name="fan_favorite"),
-    path("predictions", views.predictions, name="predictions")
+    path("predictions", views.predictions, name="predictions"),
+    path("accounts/login/", auth_views.LoginView.as_view(template_name="survive/login.html"), name="login"),
+    path("accounts/password_change/", auth_views.PasswordChangeView.as_view(template_name="survive/password_change.html"), name="password_change"),
 ]
--- web_project/settings.py
+++ web_project/settings.py
@@ -129,3 +129,5 @@ MEDIA_ROOT = [ BASE_DIR / 'media' ]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/' # on login, redirect to home page (until a profiles page exists?)
