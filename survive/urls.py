3811c5497e3c253e199331d44a735917e4444e8f
mawilson <mawilson2012@gmail.com>
2024-01-13 21:41:49 -0800

    24 01 09 users (#50)
    * Layout navbar changes including login
    
    * Added login page & URL pattern
    Added login page
    Added login to navbar
    Styled navbar as a table
    Added dummy password reset page
    
    * Added various auth & account paths
    
    * Added profile page & user auth tests to templates
    
    * Profile page additions
    Added stuff to profile page
    Added foreign key linking Teams to Users
    Refactored css
    Refactored layout so all lives in centered div
    
    * Width and border fixes
    Fixed Survivor card within content block
    Fixed table max widths
    Fixed bordered & nonbordered tables

--- survive/urls.py
+++ survive/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 from survive import views
-from survive.models import Team
-
-home_list_view = views.HomeListView.as_view(
-    queryset = Team.objects.order_by("name"), # alphabetic sort by Team Name, ascending
-    context_object_name = "team_list",
-    template_name = "survive/home.html",
-)
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    #path("", home_list_view, name="home"),
-    # path("add_team/", views.add_team, name="add_team") # deprecated
+    path("profile", views.profile, name="profile"),
     path("survivor/<int:id>/", views.survivor, name="survivor_page"),
     path("fan_favorite", views.fan_favorite, name="fan_favorite"),
-    path("predictions", views.predictions, name="predictions")
+    path("predictions", views.predictions, name="predictions"),
+
+    # account/auth paths
+    path("accounts/login/", auth_views.LoginView.as_view(template_name="survive/login.html"), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
+    path("accounts/password_change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
+    path("accounts/password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path("accounts/password_reset_confirm/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("accounts/password_reset_done", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
 ]
