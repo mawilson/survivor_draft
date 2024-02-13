f894aed0ca8c840f17b1edabafc96f5844084bbc
Matt Wilson <mawilson2012@gmail.com>
2024-02-13 13:29:16 -0800

    Added Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./survive/templates/survive/*.html", "./static_collected/survive/*.js", "./survive/static/survive/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
