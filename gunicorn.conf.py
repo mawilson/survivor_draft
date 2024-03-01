7ab0e279b43304210a036107cd3c0ad73a30a0e0
Matt Wilson <mawilson2012@gmail.com>
2024-02-29 17:38:04 -0800

    More SSL type tweaks

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,10 @@
 from web_project.settings import DEBUG
 
-bind = "0.0.0.0:5000"
+bind = "0.0.0.0:443"
 if not DEBUG:
-    keyfile = "outdraft.me_private_key.key"
-    certfile = "outdraft.me_ssl_certificate.cer"
+    keyfile = "outdraft_key.pem"
+    certfile = "outdraft_cert.pem"
+
+errorlog = "errorlog"
+loglevel = "info"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
